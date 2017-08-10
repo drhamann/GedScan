51218d93095b441c9e0036557fbc966de89279f6
George <george.hamann@gmail.com>
2017-08-10 18:01:17 -0300

    last

--- Ged/Ged/Controllers/AccountController.cs
+++ Ged/Ged/Controllers/AccountController.cs
@@ -73,6 +73,7 @@ namespace Ged.Controllers
                 return View(model);
             }
 
+
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
             var result = await SignInManager.PasswordSignInAsync(model.UserName, model.Password, model.RememberMe, shouldLockout: false);
